f8ec9c0d81ab1f0ca09b19ebc8fa8512eac51f74
luoyangwei <luoyangwei@baiying365.com>
2021-09-06 13:31:38 +0800

    demo

---
+++ wechat-mp-developers-demo/src/main/kotlin/com/mp/demo/wechatmpdevelopersdemo/WechatMpDevelopersDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.mp.demo.wechatmpdevelopersdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WechatMpDevelopersDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<WechatMpDevelopersDemoApplication>(*args)
+}
---
+++ wechat-mp-developers-demo/src/test/kotlin/com/mp/demo/wechatmpdevelopersdemo/WechatMpDevelopersDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mp.demo.wechatmpdevelopersdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WechatMpDevelopersDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
